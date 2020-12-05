c5385061305efff0e32fef72c8e994cedd7dc324
Vaansh Lakhwara <44137130+Vaansh@users.noreply.github.com>
2020-12-05 23:10:52 +0400

    Payment with Stripe API

--- php/payment/index.php
+++ php/payment/index.php
@@ -3,7 +3,13 @@ require('config.php');
 ?>
 <form action="submit.php" method="post">
 
-	<script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="<?php echo $publishableKey ?>" data-amount="1000" data-name="Beast" data-description="Payment Checkout" data-currency="usd" data-email="sphoetify.buzzness@gmail.com">
+	<script src="https://checkout.stripe.com/checkout.js" 
+		class="stripe-button" data-key="<?php echo $publishableKey ?>" 
+		data-amount="1000" 
+		data-name="Beast" 
+		data-description="Payment Checkout" 
+		data-currency="usd" 
+		data-email="">
 	</script>
 
 </form>
