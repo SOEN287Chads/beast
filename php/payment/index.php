9f2ead674f2061489d493d68c5afb4bb48531833
Vaansh Lakhwara <44137130+Vaansh@users.noreply.github.com>
2020-12-06 05:42:52 +0400

    Update index.php

--- php/payment/index.php
+++ php/payment/index.php
@@ -3,7 +3,14 @@ include('../navandfooter.html');
 require('config.php');
 ?>
 
-<script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="<?php echo $publishableKey ?>" data-amount="1000" data-name="Beast" data-description="Payment Checkout" data-currency="usd" data-email="thebeastfitteam@gmail.com">
+<script src="https://checkout.stripe.com/checkout.js" 
+        class="stripe-button" 
+        data-key="<?php echo $publishableKey ?>" 
+        data-amount="1000" 
+        data-name="Beast" 
+        data-description="Payment Checkout" 
+        data-currency="usd" 
+        data-email="thebeastfitteam@gmail.com">
 </script>
 
 </form>
