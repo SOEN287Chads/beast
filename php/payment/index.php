3fd86adb375b3828172947c8b437685e297d47ab
Vaansh Lakhwara <44137130+Vaansh@users.noreply.github.com>
2020-12-07 12:39:32 +0400

    Update index.php

--- php/payment/index.php
+++ php/payment/index.php
@@ -9,7 +9,14 @@ require('config.php');
         <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
         <center>
             <h1 style='font-family: "Monument Extended", Arial, Helvetica, sans-serif; color: white; '>Place your order: <br> <br> </h1>
-            <script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="<?php echo $publishableKey ?>" data-amount="1000" data-name="Beast" data-description="Payment Checkout" data-currency="usd" data-email="thebeastfitteam@gmail.com">
+            <script src="https://checkout.stripe.com/checkout.js" 
+                    class="stripe-button" 
+                    data-key="<?php echo $publishableKey ?>" 
+                    data-amount="1000" 
+                    data-name="Beast" 
+                    data-description="Payment Checkout" 
+                    data-currency="usd" 
+                    data-email="thebeastfitteam@gmail.com">
             </script>
         </center>
     </form>
